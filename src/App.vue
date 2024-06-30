7feceb6445eed06cdab0975bb1491daf28c75478
junjie2 <junjie304459075@gmail.com>
2024-06-30 17:41:52 +0800

    分支最后更新

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <h1>Vue3项目</h1>
-  <h2>新增的内容</h2>
-  <h2>新增的内容</h2>
-
-  <h2>新增的内容</h2>
-  <h2>新增的内容</h2>
-  <h2>新增的内容</h2>
+  <h1>我是分支最后一次更新</h1>
 </template>
 
 <script setup lang="ts"></script>
